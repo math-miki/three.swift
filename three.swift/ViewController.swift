92746f515f27c0d16485831853a5ef5d56b1f2ef
math-miki <math.micky.dtl@gmail.com>
2018-10-28 00:33:49 +0900

    first commit

---
+++ three.swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  three.swift
+//
+//  Created by 高橋幹 on 2018/10/28.
+//  Copyright © 2018 MikiTakahashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
